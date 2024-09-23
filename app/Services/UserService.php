9ddc389f41bc9ab8b9990a5e403d3e07634cd12c
Latika Joshi <itslatikajoshi@gmail.com>
2024-09-23 15:41:28 +0530

    changed home page,added new service, new controller

---
+++ app/Services/UserService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+class UserService{
+    public $name;
+    public $rollNo;
+    public $email;
+
+    // making constructor
+
+    function __construct(){}
+
+        static public function getName()
+        {
+            return "Latika";
+
+        }
+
+
+    }
